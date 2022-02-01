161ba61a14ebd4f9a55021cfcc73cb5a7ff81a59
bobbobbob15 <wd40bug@gmail.com>
2022-02-01 13:58:52 -0700

    printing to standard err added

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ fn main(){
     let args:Vec<String> = env::args().collect();
 
     let arguments = Config::new(&args).unwrap_or_else(|err| {
-        println!("problem parsing file: {}",err);
+        eprintln!("problem parsing arguments {}",err);
         process::exit(1);
     });
 
     if let Err(e) = command_line::run(arguments){
-        println!("Application error: {}",e);
+        eprintln!("Application error: {}",e);
         process::exit(1);
     }
     
